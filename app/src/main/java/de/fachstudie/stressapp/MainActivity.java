0126eafabe27a5798adb7f7f39e541b90e6ac5bf
paul.kuznecov <mail@paul-kuznecov.de>
2016-11-15 19:42:30 +0100

    Add bar chart to main activity

--- app/src/main/java/de/fachstudie/stressapp/MainActivity.java
+++ app/src/main/java/de/fachstudie/stressapp/MainActivity.java
@@ -9,20 +9,39 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import de.fachstudie.stressapp.databinding.ActivityMainBinding;
 import de.fachstudie.stressapp.db.DatabaseHelper;
 import de.fachstudie.stressapp.model.StressNotification;
+import de.fachstudie.stressapp.model.ViewNotification;
 
 public class MainActivity extends AppCompatActivity {
-    private StressNotification notification;
+    private ViewNotification notification;
     private NotificationReceiver notificationReceiver;
     private IntentFilter filter;
+    private BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +51,11 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        notification = new StressNotification();
+        notification = new ViewNotification();
         notification.title.set("Title");
         notification.content.set("Content");
         notification.application.set("Application");
+        notification.timestamp.set("Timestamp");
         binding.setCurrentNotification(notification);
 
         notificationReceiver = new NotificationReceiver();
@@ -45,32 +65,88 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        String[] projection = {StressNotification.NotificationEntry._ID, StressNotification
-                .NotificationEntry.TITLE, StressNotification.NotificationEntry.APPLICATION,
-                StressNotification.NotificationEntry.TIMESTAMP, StressNotification
+        String[] projection = {ViewNotification.NotificationEntry._ID, ViewNotification
+                .NotificationEntry.TITLE, ViewNotification.NotificationEntry.APPLICATION,
+                ViewNotification.NotificationEntry.TIMESTAMP, ViewNotification
                 .NotificationEntry.CONTENT};
-        Cursor c = db.query(StressNotification.NotificationEntry.TABLE_NAME, projection, null,
+        Cursor c = db.query(ViewNotification.NotificationEntry.TABLE_NAME, projection, null,
                 null, null, null, null);
 
-        if (c != null) {
-            c.moveToFirst();
-            String dbTitle = c.getString(c.getColumnIndex(StressNotification.NotificationEntry
-                    .TITLE));
-            String dbContent = c.getString(c.getColumnIndex(StressNotification.NotificationEntry
-                    .CONTENT));
-            String dbApplication = c.getString(c.getColumnIndex(StressNotification.NotificationEntry
-                    .APPLICATION));
-            String timeStamp = c.getString(c.getColumnIndex(StressNotification.NotificationEntry
-                    .TIMESTAMP));
-            Log.d("Timestamp", timeStamp);
+        List<StressNotification> notificationList = new ArrayList<>();
+        if (c != null && c.moveToFirst()) {
+            Log.d("Count: ", c.getCount() + "");
 
-            notification.title.set(dbTitle);
-            notification.content.set(dbContent);
-            notification.application.set(dbApplication);
+            while (c.moveToNext()) {
+                String title = c.getString(c.getColumnIndex(ViewNotification.NotificationEntry
+                        .TITLE));
+                String content = c.getString(c.getColumnIndex(ViewNotification.NotificationEntry
+                        .CONTENT));
+                String application = c.getString(c.getColumnIndex(ViewNotification.NotificationEntry
+                        .APPLICATION));
+                String timeStampText = c.getString(c.getColumnIndex(ViewNotification
+                        .NotificationEntry
+                        .TIMESTAMP));
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date timeStampDate = null;
+                try {
+                    timeStampDate = sdf.parse(timeStampText);
+                } catch (ParseException e) {
+                }
+
+                StressNotification not = new StressNotification(title, application, content,
+                        timeStampDate);
+                notificationList.add(not);
+            }
 
             db.close();
             dbHelper.close();
         }
+
+        barChart = (BarChart) findViewById(R.id.chart);
+        barChart.getXAxis().setDrawGridLines(false);
+        Description d = new Description();
+        d.setText("Stunde");
+        barChart.setDescription(d);
+
+        Map<Integer, Integer> dayHourToNotificationCount = new HashMap<>();
+        for (int i = 0; i < 24; i++) {
+            dayHourToNotificationCount.put(i, 0);
+        }
+
+        for (StressNotification n : notificationList) {
+            Date timestamp = n.getTimestamp();
+            if (timestamp == null) {
+                continue;
+            }
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(timestamp);
+            int hourOfDay = cal.get(Calendar.HOUR_OF_DAY);
+            dayHourToNotificationCount.put(hourOfDay,
+                    dayHourToNotificationCount.get(hourOfDay) + 1);
+        }
+        List<BarEntry> entries = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            BarEntry entry = new BarEntry(i, dayHourToNotificationCount.get(i));
+            entries.add(entry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, "# Notifications");
+        barDataSet.setColor(ContextCompat.getColor(this, R.color.barChartColor));
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barChart.invalidate();
+
+        StressNotification last = notificationList.get(notificationList.size() - 1);
+        notification.title.set(last.getTitle());
+        notification.content.set(last.getContent());
+        notification.application.set(last.getApplication());
+        notification.timestamp.set(last.getTimestamp().toString());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //lineChart.invalidate();
     }
 
     @Override
@@ -111,17 +187,20 @@ public class MainActivity extends AppCompatActivity {
             String title = intent.getStringExtra("title");
             String content = intent.getStringExtra("content");
             String application = intent.getStringExtra("application");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timestamp = dateFormat.format(new Date());
 
             notification.title.set(title);
             notification.content.set(content);
             notification.application.set(application);
+            notification.timestamp.set(timestamp);
 
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             ContentValues values = new ContentValues();
-            values.put(StressNotification.NotificationEntry.TITLE, title);
-            values.put(StressNotification.NotificationEntry.CONTENT, content);
-            values.put(StressNotification.NotificationEntry.APPLICATION, application);
-            db.insert(StressNotification.NotificationEntry.TABLE_NAME, null, values);
+            values.put(ViewNotification.NotificationEntry.TITLE, title);
+            values.put(ViewNotification.NotificationEntry.CONTENT, content);
+            values.put(ViewNotification.NotificationEntry.APPLICATION, application);
+            db.insert(ViewNotification.NotificationEntry.TABLE_NAME, null, values);
         }
     }
 }
--- app/src/main/java/de/fachstudie/stressapp/model/StressNotification.java
+++ app/src/main/java/de/fachstudie/stressapp/model/StressNotification.java
@@ -1,22 +1,53 @@
 package de.fachstudie.stressapp.model;
 
-import android.databinding.ObservableField;
-import android.provider.BaseColumns;
+import java.util.Date;
 
 /**
- * Created by Paul Kuznecov on 08.11.2016.
+ * Created by Paul Kuznecov on 13.11.2016.
  */
 public class StressNotification {
-    public final ObservableField<String> content = new ObservableField<>();
-    public final ObservableField<String> title = new ObservableField<>();
-    public final ObservableField<String> application = new ObservableField<>();
 
-    public static class NotificationEntry implements BaseColumns {
-        public static final String TABLE_NAME = "notification";
-        public static final String TITLE = "title";
-        public static final String CONTENT = "content";
-        public static final String APPLICATION = "application";
-        public static final String TIMESTAMP = "timestamp";
+    private String title;
+    private String application;
+    private String content;
+    private Date timestamp;
 
+    public StressNotification(String title, String application, String content, Date timestamp) {
+        this.title = title;
+        this.application = application;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
     }
 }
