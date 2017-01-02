a139299a05e06dd53cbd242fa5cd54cd5e0f76eb
Sanjeev <sanjeev.b@gmx.de>
2017-01-02 11:45:46 +0100

    add remaining questions of PSS

--- app/src/main/java/de/fachstudie/stressapp/SurveyActivity.java
+++ app/src/main/java/de/fachstudie/stressapp/SurveyActivity.java
@@ -7,6 +7,10 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SurveyActivity extends AppCompatActivity {
 
@@ -28,15 +32,26 @@ public class SurveyActivity extends AppCompatActivity {
         if (requestCode == SURVEY_REQUEST) {
             if (resultCode == RESULT_OK) {
 
-                String answers_json = data.getExtras().getString("answers");
-                Log.d("****", "****************** WE HAVE ANSWERS ******************");
-                Log.v("ANSWERS JSON", answers_json);
-                Log.d("****", "*****************************************************");
+                String json_result = data.getExtras().getString("answers");
+                Log.v("JSON RESULT", json_result);
 
-                //do whatever you want with them...
+                List<String> answers = getSurveyAnswers(json_result);
+
+                // TODO save answers in DB
             }
         }
     }
+
+    private List<String> getSurveyAnswers(String answers_json) {
+        List<String> answers = new ArrayList<>();
+        Pattern pattern = Pattern.compile(":(.*?)(,|\\})");
+        Matcher matcher = pattern.matcher(answers_json);
+        while(matcher.find()){
+            answers.add(matcher.group(1));
+        }
+        return answers;
+    }
+
     //json stored in the assets folder. but you can get it from wherever you like.
     private String loadSurveyJson(String filename) {
         try {
