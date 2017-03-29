0a78e5686d20718a840c1341f59b1ea0be2daaa1
Sanjeev <sanjeev.b@gmx.de>
2017-03-29 21:08:00 +0200

    Rename notification listener service

--- app/src/main/java/org/hcilab/projects/stressblocks/NLRegistryService.java
+++ app/src/main/java/org/hcilab/projects/stressblocks/NLRegistryService.java
@@ -24,7 +24,7 @@ import java.util.Date;
  * Created by Paul Kuznecov on 01.11.2016.
  */
 
-public class NLService extends NotificationListenerService {
+public class NLRegistryService extends NotificationListenerService {
 
     private NotificationReceiver notificationReceiver;
 
